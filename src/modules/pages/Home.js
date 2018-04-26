672f738a3d4febf8aeda931989e10437dc0af3e3
Li <KevinLi@Scholastic.com>
2018-04-26 16:20:52 -0400

    - looks kinda better

--- src/modules/pages/Home.js
+++ src/modules/pages/Home.js
@@ -4,7 +4,7 @@ export default class Home extends React.Component {
 
 	render() {
 
-		return (<div>
+		return (<div className="register-form mdc-elevation--z8">
 			Home
 			<button className="foo-button mdc-button mdc-button--raised">
 				<i className="material-icons mdc-button__icon" aria-hidden="true">favorite</i>
