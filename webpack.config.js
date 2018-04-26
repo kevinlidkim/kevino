8482652a38dabd9f0fdee6406ad9cc07c57e9b3b
Li <KevinLi@Scholastic.com>
2018-04-26 13:18:55 -0400

    - added sass configuration for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,45 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      use: ['babel-loader'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+	devtool: 'eval',
+	entry: {
+		home: [
+			'./src/index.js',
+			'./src/styles/app.scss'
+		]
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/dist/'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				use: [ 'babel-loader'],
+				include: path.join(__dirname, 'src')
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'bundle.css'
+						}
+					},
+					{ loader: 'extract-loader' },
+					{ loader: 'css-loader' },
+					{ loader: 'sass-loader' }
+				]
+			}
+		]
+	}
 };
